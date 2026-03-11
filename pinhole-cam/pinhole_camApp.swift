a632a95bc16636dd0d16e3b9dbbd34f45838e398
“Extell <efvalerius@gmail.com>
2026-03-11 11:30:32 -0500

    Initial Commit

---
+++ pinhole-cam/pinhole_camApp.swift
@@ -0,0 +1,17 @@
+//
+//  pinhole_camApp.swift
+//  pinhole-cam
+//
+//  Created by Gilgamesh Craos on 3/11/26.
+//
+
+import SwiftUI
+
+@main
+struct pinhole_camApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pinhole-camTests/pinhole_camTests.swift
@@ -0,0 +1,17 @@
+//
+//  pinhole_camTests.swift
+//  pinhole-camTests
+//
+//  Created by Gilgamesh Craos on 3/11/26.
+//
+
+import Testing
+@testable import pinhole_cam
+
+struct pinhole_camTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
