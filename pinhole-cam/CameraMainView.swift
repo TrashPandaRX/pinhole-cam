56e087b711296861996297f2e87efd570d72a315
“Extell <efvalerius@gmail.com>
2026-04-14 22:16:12 -0500

    adding in some of the swiftui logic

--- pinhole-cam/CameraMainView.swift
+++ pinhole-cam/CameraMainView.swift
@@ -12,8 +12,38 @@ struct CameraMainView: View {
 
     var body: some View {
         ZStack {
-			
-        }
+			// darker background for the camera
+			Color.black.ignoresSafeArea()
+
+			if cameraManager.isCameraAuthorized {
+				// 1. the preview layer in the background
+				CameraPreviewView(session: cameraManager.session)
+					.ignoresSafeArea()
+					.onAppear() {
+						cameraManager.startSession()
+					}
+					.onDisappear() {
+						cameraManager.stopSession()
+					}
+				// 2. overlaid ui elements
+				VStack {
+					Spacer()
+					
+					// camera controls
+					HStack{
+						Spacer()
+						
+						Button(action: {
+							cameraManager.capturePhoto()
+						}) {
+							Circle()
+								.stroke(Color.white, lineWidth: 3)
+								.frame(width: 70, height: 70)
+						}
+					}
+				}
+			}
+		}
     }
 }
 
