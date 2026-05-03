3fc8140e9603f38bc673e33f24362d2ee014721a
TrashPandaRX <efvalerius@gmail.com>
2026-05-02 20:13:41 -0500

    Initial Commit

---
+++ ColorDetector/ColorDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorDetectorApp.swift
+//  ColorDetector
+//
+//  Created by Extell Farve on 5/2/26.
+//
+
+import SwiftUI
+
+@main
+struct ColorDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorDetector/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColorDetector
+//
+//  Created by Extell Farve on 5/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
